e94380e853840b023da962cd7c74ab642daa63b3
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2022-11-21 21:03:27 +0900

    Display updating frequency ratio

--- examples/sum_of_sines.jl
+++ examples/sum_of_sines.jl
@@ -4,7 +4,6 @@ set_theme!(theme_retina())
 
 x = 0:0.01:8π
 obs = [Observable(0.0) for s in 1:6]
-
 sine(x, a, ω, ϕ) = @. a * sin((ω * x + ϕ) * π)
 
 y1 = @lift(sine(x, $(obs[1]), $(obs[2]), $(obs[3])))
@@ -23,13 +22,19 @@ sg = SliderGrid(fig[1:2, 2][1, 1],
     (label = L"δ_2", range = -6:0.01:6, format = "{:0.1f}π", startvalue = 0.5),
     tellheight = false,
 )
-lb = Label(fig[1:2, 2][2, 1], "ω_1 - ω_2 = UPDATE HERE", tellwidth = false)
+
 
 sliderobservables = [s.value for s in sg.sliders]
 
 for (i, o) in enumerate(obs)
     connect!(o, sliderobservables[i])
 end
+
+label = lift(sliderobservables[2], sliderobservables[5]) do s1, s2
+    return "ω_2 / ω_1 = $(round(s2 / s1, digits=2))"
+end
+
+lb = Label(fig[1:2, 2][2, 1], label, tellwidth = false)
 
 # xtickvals = range(0, 4π, length = 9)
 # xtickformat = ["0", "π/2", "π", "3π/2", "2π", "5π/2", "3π", "7π/2", "4π"]
