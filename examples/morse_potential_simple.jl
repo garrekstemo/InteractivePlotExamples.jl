0684b4902c184acb06e9f432947de6115f6885fe
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2022-11-27 07:43:13 +0900

    Add toggles

--- examples/morse_potential_simple.jl
+++ examples/morse_potential_simple.jl
@@ -1,3 +1,5 @@
+# J. P. Dahl and M. Springborg, The Morse Oscillator in Position Space, Momentum Space, and Phase Space, The Journal of Chemical Physics 88, 4535 (1988).
+
 using GLMakie
 using SomeMakieThemes
 set_theme!(theme_retina())
@@ -17,36 +19,52 @@ function endpoints(energy, λ)
     return left, right
 end
 
-q = -10:0.01:30
 λ = Observable(5)
-ns = @lift(1:$λ)
+ns = @lift(0:$λ)
+q = -10:0.01:30
+
 morse = @lift(morse_potential(q, $λ))
 energies = @lift(morse_energies($ns, $λ))
+harmonic_energies = @lift(@. sqrt(2 * ($ns + 0.5)))
 
 fig = Figure(resolution = (1500, 900))
 display(fig)
 
-eq = Label(fig, L"V(q) = \frac{\lambda}{2}( 1 - e^{-q / \sqrt{\lambda}} )")
+eq1 = Label(fig, L"V_m(q) = \frac{\lambda}{2}( 1 - e^{-q / \sqrt{\lambda}} )")
+eq2 = Label(fig, L"V_h(q) = \frac{1}{2}q^2")
+
+toggle = Toggle(fig, active = false)
+tlabel = Label(fig, "Harmonic on/off")
 slabel = Label(fig, @lift("λ = $($λ)"))
+
 s = Slider(fig, range = 1:20, startvalue = 5, width = 300)
 connect!(λ, s.value)
 
-fig[1, 2] = vgrid!(
-    eq,
+fig[1, 2][1, 1] = vgrid!(eq1, eq2, tellheight = false)
+fig[1, 2][2, 1] = vgrid!(
     slabel,
     s,
     tellheight = false,
     width = 500
-)
-
+    )
+fig[1, 2][3, 1] = grid!(hcat(toggle, tlabel), tellheight = false)
+    
 ax = Axis(fig[1, 1], title = "Simple Morse Oscillator Model", xlabel = "q", ylabel = "Energy")
 
 lines!(q, morse)
+lines_h = lines!(q, 0.5 * q.^2)
 
 xlims!(-7, 25)
-ylims!(-1, 13)
+ylims!(-1, 15)
 
 xrange = @lift($(ax.limits)[1][2] - $(ax.limits)[1][1])
-xmin = @lift((endpoints.($energies, $λ)[end][1] - $(ax.limits)[1][1]) / $xrange)
-xmax = @lift((endpoints.($energies, $λ)[end][2] - $(ax.limits)[1][1]) / $xrange)
-hlines!(@lift($energies[end]), xmin = xmin, xmax = xmax)
+
+xmin_m = @lift((endpoints.($energies, $λ)[end][1] - $(ax.limits)[1][1]) / $xrange)
+xmax_m = @lift((endpoints.($energies, $λ)[end][2] - $(ax.limits)[1][1]) / $xrange)
+xmin_h = @lift((-$(harmonic_energies)[end] - $(ax.limits)[1][1]) / $xrange)
+xmax_h = @lift(($(harmonic_energies)[end] - $(ax.limits)[1][1]) / $xrange)
+
+hlines!(@lift($energies[end]), xmin = xmin_m, xmax = xmax_m)
+hlines_h = hlines!(@lift((@. $ns + 0.5)[end]), xmin = xmin_h, xmax = xmax_h, visible = false)
+connect!(lines_h.visible, toggle.active)
+connect!(hlines_h.visible, toggle.active)
