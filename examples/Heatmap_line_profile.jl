ecb5ef93fe3977640c534428ab3a65364d9b6f3c
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2024-05-23 18:13:37 +0900

    Oops, didn't merge last commit correctly

--- examples/Heatmap_line_profile.jl
+++ examples/Heatmap_line_profile.jl
@@ -9,58 +9,36 @@ end
 x = range(-100, 100, step=0.5)
 y = range(-100, 100, step=0.5)
 p = [1, 0, 0, 20, 30]
+z = zeros(Float64, length(x), length(y))
 
-# Configure the settings for heatmap
-f = Figure(resolution = (1200,2000),fontsize = 30)
-display(f)
-DataInspector(f)
-ax1 = Axis(f[1,1],title = "2D Gaussian function",xlabel  = "x",xlabelsize = 40,ylabel = "y",ylabelsize = 40)
-
-
-# Create plot data and 2D gaussian parameters
-x = range(1,200,2000)
-y = range(1,100,1000)
-p = [50,100,50,10,10]
-
-
-# Create z data for heatmap based on x, y, and p
-z = zeros(Float64,length(x),length(y))
-
-for (i,j) in enumerate(x)
-    for (m,n) in enumerate(y)
-        z[i,m] = gaussian(p,j,n)
+for (i, j) in enumerate(x)
+    for (m, n) in enumerate(y)
+        z[i, m] = gaussian(p, j, n)
     end
 end
 
-
-# Plot heatmap
-heatmap!(ax1,x,y,z,interactivity = true)
-
-
-# Configure the settings for 2D figures
-ax2 = Axis(f[2,1],title = "Gaussian function x",xlabel  = "x",xlabelsize = 40,ylabel = "z",ylabelsize = 40)
-ax3 = Axis(f[3,1],title = "Gaussian function y",xlabel  = "y",xlabelsize = 40,ylabel = "z",ylabelsize = 40)
-
+# Create figure
+fig = Figure(size = (450, 900))
+DataInspector()
 
 # Define observables
 x_line = Observable(100.0)
 y_line = Observable(50.0)
+xdata = Observable(z[:, div(length(y), 2)])
+ydata = Observable(z[div(length(x), 2), :])
 
-
-ax1 = Axis(fig[1, 1], title = "2D Gaussian function", xlabel  = "x", ylabel = "y")
+# Plot heatmap and cursor lines
+ax1 = Axis(fig[1,1], title = "2D Gaussian function", xlabel = "x", ylabel = "y")
 heatmap!(x, y, z)
-vlines!(hline, color = :red)
-hlines!(vline, color = :red)
+vlines!(x_line, color = :red)
+hlines!(y_line, color = :red)
 
+# Plot cuts of the 2D data
+ax2 = Axis(fig[2, 1], title = "Cut along x-axis", xlabel = "x", ylabel = "z")
+ax3 = Axis(fig[3, 1], title = "Cut along y-axis", xlabel = "y", ylabel = "z")
 
-# Plot straight lines indicating the position of the cursor
-vlines!(ax1,x_line,color = :red)
-hlines!(ax1,y_line,color = :red)
-
-
-# Plot 2D figures
-lines!(ax2,x,x_data)
-lines!(ax3,y,y_data)
+lines!(ax2, x, xdata)
+lines!(ax3, y, ydata)
 
 
 # on-do Block(executed when triggered)
@@ -68,8 +46,8 @@ on(events(ax1).mouseposition) do mpos
     xpos = mouseposition(ax1.scene)[1]
     ypos = mouseposition(ax1.scene)[2]
 
-    hline[] =  trunc(Int,mouseposition(ax1.scene)[1])
-    vline[] =  trunc(Int,mouseposition(ax1.scene)[2])
+    x_line[] =  trunc(Int,mouseposition(ax1.scene)[1])
+    y_line[] =  trunc(Int,mouseposition(ax1.scene)[2])
     xdata[] = z[:, findfirst(isapprox(ypos, atol = 0.5), y)]
     ydata[] = z[findfirst(isapprox(xpos, atol = 0.5), x), :]
 end
